716aaa3dafa12d46eca9d19aa1d42a4ddb0026bc
viral390 <viralpy1@gmail.com>
2021-02-03 15:14:46 +0530

    making models

--- my_cart/my_cart/settings.py
+++ my_cart/my_cart/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
-    'blog'
+    'shop.apps.ShopConfig',
+    'blog.apps.BlogConfig'
 
 ]
 
