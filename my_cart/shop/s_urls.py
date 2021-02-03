7173f0a2d56d3e541c8e85a4fc2436de34467055
viral390 <viralpy1@gmail.com>
2021-02-03 14:23:43 +0530

    adding url of shop app

--- my_cart/shop/s_urls.py
+++ my_cart/shop/s_urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name="shophome")
+    path('', views.index, name="shophome"),
+    path('about/', views.about, name="about as"),
+    path('contact/', views.contact, name="contact us"),
+    path('search/', views.search, name="search"),
+    path('productviews/', views.productviews, name="product views"),
+    path('checkout/', views.checkout, name="check out"),
+    path('tracker/', views.tracker, name="tracking status")
 ]
--- my_cart/shop/views.py
+++ my_cart/shop/views.py
@@ -5,3 +5,24 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("this is my about page")
+
+
+def contact(request):
+    return HttpResponse("this is my contact page")
+
+def search(request):
+    return HttpResponse("this is my search page")
+
+def productviews(request):
+    return HttpResponse("this is my product views page")
+
+
+def checkout(request):
+    return HttpResponse("this is my checkout  page")
+
+
+def tracker(request):
+    return HttpResponse("this is my tracker page")
