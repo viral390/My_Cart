d87b7baaac9350d342befd76c4d2e18e4dc8e60b
viral390 <viralpy1@gmail.com>
2021-02-04 16:08:03 +0530

    adding photos of products and also work on models

--- my_cart/shop/models.py
+++ my_cart/shop/models.py
@@ -6,4 +6,11 @@ class product(models.Model):
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="shop/images", default="")
 
+
+    def __str__(self):
+        return self.product_name
